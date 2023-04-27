3eef072a970baea19142b0513398e982685d4ad8
xueyi <15827543830@163.com>
2023-04-28 00:51:44 +0800

    update plugin

--- plugins/search/app/main.py
+++ plugins/search/app/main.py
@@ -1,6 +1,6 @@
 import os
 from fastapi import FastAPI
-from langchain.tools import DuckDuckGoSearchTool
+from langchain.tools import DuckDuckGoSearchRun
 from pydantic import BaseModel
 
 os.environ["HTTP_PROXY"] = "http://192.168.1.202:1081"
@@ -21,7 +21,7 @@ class SearchResponse(BaseModel):
 
 @app.post("/chat", response_model=SearchResponse)
 async def root(item: SearchQuery):
-    tools = DuckDuckGoSearchTool()
+    tools = DuckDuckGoSearchRun()
     print("收到 info %s" % item.command)
     res = tools.run(item.command)
     print("返回 info %s" % res)
